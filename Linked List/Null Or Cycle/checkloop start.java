2288d8e8c4a96a029b9421640998553e3c427483
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-10-29 14:37:23 +0530

    If it a cycle find the start node of the loop

---
+++ Linked List/Null Or Cycle/checkloop start.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package competitive.programming;
+
+/**
+ *
+ * @author DELL
+ */
+class Noder 
+{
+    int data;
+    Noder next;
+}
+public class prob11 {
+public static void main(String args[])
+{
+    Noder node1 = new Noder();
+        Noder node2 = new Noder();
+        Noder node3 = new Noder();
+        Noder node4 = new Noder();
+        Noder node5 = new Noder();
+        Noder node6 = new Noder();
+        node1.data = 1;
+        node2.data = 2;
+        node3.data = 3;
+        node4.data = 4;
+        node5.data = 5;
+        node6.data = 6;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+       // node6.next = node3;
+        if(check(node1)==true)
+        {
+            System.out.println("snail");
+            
+        }
+        else
+        {
+            
+            System.out.println("SnaKe");
+        }
+		
+}
+    static boolean check(Noder head)
+    {
+         Noder temp1=head;
+        Noder temp2=head;
+int lop=0;
+        while(true)        
+        {
+            if(temp2==null||temp2.next==null)
+            {
+                lop=0;
+                break;
+            }
+            temp1=temp1.next;
+            temp2=temp2.next.next;
+               if(temp1==temp2)
+            {   
+            lop=1;
+            break;
+            }
+            
+        }
+        if(lop==1)
+        {
+            temp1=head;
+            while(temp1!=temp2)
+            {
+            temp1=temp1.next;
+            temp2=temp2.next;
+            }
+                System.out.println(temp1.data);
+            return true;
+            }
+        return false;
+        }
+        
+
+    }
