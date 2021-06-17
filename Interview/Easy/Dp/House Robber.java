5abae6669f2a9f69ba68a851aa36377bd68abca0
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-06-17 19:48:29 +0530

    Update House Robber.java

--- Interview/Easy/Dp/House Robber.java
+++ Interview/Easy/Dp/House Robber.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            dp[i+1]=Math.max(dp[i-1]+nums[i],dp[i]);
+        }
+        return dp[n];
+    }
+}
