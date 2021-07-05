a2cc58a2c3628f543b2879a49c3898edc7d72b6f
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-07-05 23:17:14 +0530

    Update Group Anagrams.java

--- Interview/Medium/Arrays & Strings/Group Anagrams.java
+++ Interview/Medium/Arrays & Strings/Group Anagrams.java
@@ -1,41 +1,22 @@
 class Solution {
-    public String sortString(String s)
-    {
-        char a[]=s.toCharArray();
-        Arrays.sort(a);
-        return new String(a);//return new Sorted string
-    }
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res= new ArrayList<>();
-        
-        Set<String> se=new HashSet<>();
-        int n=strs.length;
-        String s[]=new String[n];
-        for(int i=0;i<n;i++)
-        {
-            s[i]=strs[i];
-        }
-        for(int i=0;i<n;i++)
-        {
-           strs[i]= sortString(strs[i]);
-            se.add(strs[i]);   
-        }
-        
-        for(String i:se)
-        {
-            List<String> a=new ArrayList<>();
-            for(int j=0;j<n;j++)
-            { 
-                if(strs[j].equals(i))
-                {
-                    a.add(s[j]);
-                }
+        HashMap<String,List<String>> h=new HashMap<>();
+        List<List<String>> b=new ArrayList<>();
                 
+        for(int i=0;i<strs.length;i++)
+        {
+         char ch[]= strs[i].toCharArray();
+            Arrays.sort(ch);
+            String s=String.valueOf(ch);
+            if(!h.containsKey(s))
+            {
+                h.put(s,new ArrayList<>());
             }
-            res.add(a);
+            h.get(s).add(strs[i]);
+                
         }
-        
-        
-        return res;
+        b.addAll(h.values());
+        return b;
+
     }
 }
