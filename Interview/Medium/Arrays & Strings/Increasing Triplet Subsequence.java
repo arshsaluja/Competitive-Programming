2e7cf577f6fff202fa19a6eb2c9fb082df59a585
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-07-11 23:34:57 +0530

    Update Increasing Triplet Subsequence.java

--- Interview/Medium/Arrays & Strings/Increasing Triplet Subsequence.java
+++ Interview/Medium/Arrays & Strings/Increasing Triplet Subsequence.java
@@ -1,24 +1,15 @@
+//[1,2,3,4,5]
 class Solution {
     public boolean increasingTriplet(int[] nums) {
+        int i=Integer.MAX_VALUE;int k=Integer.MAX_VALUE;
         int n=nums.length;
-        if(n<3) return false;
-       int lm[]=new int[n];//smallest ele
-        int rm[]=new int[n];//largest ele
-        lm[0]=nums[0];
-        for(int i=1;i<n;i++)
+        for(int j=0;j<n;j++)
         {
-            lm[i]=Math.min(lm[i-1],nums[i]);
-            System.out.println("lm"+lm[i]);
-        }
-        rm[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            rm[i]=Math.max(rm[i+1],nums[i]);
-             System.out.println("rm"+rm[i]);
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(lm[i]<nums[i]&&nums[i]<rm[i])
+            if(i>=nums[j])
+                i=nums[j];
+            else if(k>=nums[j])
+                k=nums[j];
+            else
                 return true;
         }
         return false;
