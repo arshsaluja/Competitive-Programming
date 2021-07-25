9737959e09b9c40c2270f3e759b539eb9c6f0813
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-07-25 18:33:54 +0530

    Update Number of Islands.java

--- Interview/Medium/Trees &Graphs/Number of Islands.java
+++ Interview/Medium/Trees &Graphs/Number of Islands.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    public void dfs(char[][] grid,int i,int j)
+    {
+        if(i<0||i>=grid.length||j<0||j>=grid[i].length||grid[i][j]=='0')
+            return;
+        grid[i][j]='0';
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+    }
+    
+    public int numIslands(char[][] grid) {
+        int c=0;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[i].length;j++)
+            {
+                if(grid[i][j]=='1'){
+                    c++;
+                dfs(grid,i,j);
+                }
+            }
+        }
+        return c;
+    }
+}
