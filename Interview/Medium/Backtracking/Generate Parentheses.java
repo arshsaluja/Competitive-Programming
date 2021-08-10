445bf4f1ebf4ae73ce088e08ba75e1e98a0b67f9
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-10 21:59:10 +0530

    Update Generate Parentheses.java

--- Interview/Medium/Backtracking/Generate Parentheses.java
+++ Interview/Medium/Backtracking/Generate Parentheses.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> m=new ArrayList<>();
+        pp(m,"",0,0,n);
+        return m;
+    }
+    public void pp(List<String> m,String s,int open,int close,int max)
+    {
+        if(s.length()==max*2)
+        {
+            m.add(s);
+            return ;
+        }
+        if(open<max)pp(m,s+"(",open+1,close,max);
+        if(close<open)pp(m,s+")",open,close+1,max);
+    }
+}
