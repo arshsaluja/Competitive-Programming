468b461fbe83e936b73ed68d4c2ea5f6e1888ba8
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-11 22:02:41 +0530

    Update Permutations.java

--- Interview/Medium/Backtracking/Permutations.java
+++ Interview/Medium/Backtracking/Permutations.java
@@ -1,1 +1,29 @@
-
+class Solution {
+    public List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> cur=new ArrayList<>();
+        int n=nums.length;
+        boolean visited[]=new boolean[n];
+        solve(cur,nums,visited,n);
+        return res;
+    }
+    public boolean solve(List<Integer> cur,int nums[],boolean visited[],int n)
+    {
+        if(cur.size()==n)
+        {
+            res.add(new ArrayList(cur));
+            return true;
+        }
+       
+            for(int i=0;i<n;i++)
+            {
+                if(visited[i]) continue;
+                cur.add(nums[i]);
+                visited[i]=true;
+                solve(cur,nums,visited,n);
+                cur.remove(cur.size()-1);
+                visited[i]=false;
+            }
+        return false;
+    }
+}
