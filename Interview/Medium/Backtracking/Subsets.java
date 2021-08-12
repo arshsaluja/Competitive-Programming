64457af00ec46e97ac5027479bfee9e45a8738d7
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-12 23:36:01 +0530

    Update Subsets.java

--- Interview/Medium/Backtracking/Subsets.java
+++ Interview/Medium/Backtracking/Subsets.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> r=new ArrayList<>();
+        List<List<Integer>> res=new ArrayList<>();
+        int n=nums.length;
+        int i=0;
+        solve(r,res,nums,i);
+        return res;
+            
+    }
+    public void solve( List<Integer> r,List<List<Integer>> res,int[] nums,int i)
+    {
+        if(i==nums.length)
+        {
+            res.add(new ArrayList<>(r));
+            return ;
+        }
+        r.add(nums[i]);
+        solve(r,res,nums,i+1);
+        r.remove(r.size()-1);
+        solve(r,res,nums,i+1);
+    }
+    
+}
