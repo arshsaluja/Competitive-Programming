622bddc1fbd93969ad01d4852f4aa4cc4b45f3b1
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-13 22:30:01 +0530

    Update Word Search.java

--- Interview/Medium/Backtracking/Word Search.java
+++ Interview/Medium/Backtracking/Word Search.java
@@ -1,1 +1,35 @@
-
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        return solve(board,word);
+    }
+    public boolean rec(char[][] board,int row,int col,int k,String s)
+    {
+        if(k==s.length())
+            return true;
+        if(row==-1||col==-1||col==board[0].length||row==board.length||board[row][col]!=s.charAt(k))
+            return false;
+        char temp=board[row][col];
+        board[row][col]='*';
+            boolean t=rec(board,row-1,col,k+1,s)||
+                rec(board,row,col-1,k+1,s)||
+                rec(board,row+1,col,k+1,s)||
+                rec(board,row,col+1,k+1,s);
+         board[row][col]=temp;
+        return t;
+    }
+    public boolean solve(char board[][],String s)
+    {
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[0].length;j++)
+            {
+                if(board[i][j]==s.charAt(0)&&rec(board,i,j,0,s))
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
