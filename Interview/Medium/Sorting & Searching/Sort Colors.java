6031d83f50d29c81c881502472672957b9881043
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-16 20:21:55 +0530

    Update Sort Colors.java

--- Interview/Medium/Sorting & Searching/Sort Colors.java
+++ Interview/Medium/Sorting & Searching/Sort Colors.java
@@ -1,5 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int i0=0;
+        int i1=0;
+        int i2=0;
+        int n=nums.length;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)i0++;
+            else if(nums[i]==1)i1++;
+            else if(nums[i]==2)i2++;
+        }
+        int k=0;
+         for(int i=0;i<i0;i++)
+        {
+            nums[i]=0;
+        }
+         for(int i=i0;i<i0+i1;i++)
+        {
+            nums[i]=1;
+        }
+         for(int i=i0+i1;i<n;i++)
+        {
+            nums[i]=2;
+        }
+        
     }
 }
