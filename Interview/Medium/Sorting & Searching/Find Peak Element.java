fe5a2e816dc56171fb84c484f910bc6c06b84561
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-18 17:09:36 +0530

    Update Find Peak Element.java

--- Interview/Medium/Sorting & Searching/Find Peak Element.java
+++ Interview/Medium/Sorting & Searching/Find Peak Element.java
@@ -1,1 +1,32 @@
-
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        if(n<=1)
+        {
+            return 0;
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i==0&&nums[i]>nums[i+1])
+            {
+                c=i;
+                break;
+            }
+            if(i==n-1&&nums[i]>nums[i-1])
+            {
+                c=i;
+                break;
+            }
+            else
+            {
+                if(i!=0&&i!=n-1&&nums[i]>nums[i-1]&&nums[i]>nums[i+1])
+                {
+                    c=i;
+                break;
+                }
+            }
+        }
+        return c;
+    }
+}
