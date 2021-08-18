b34ce5dc63b23b4c570d64fab35dab668e2840e4
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-18 11:41:49 +0530

    Update Kth Largest Element in an Array.java

--- Interview/Medium/Sorting & Searching/Kth Largest Element in an Array.java
+++ Interview/Medium/Sorting & Searching/Kth Largest Element in an Array.java
@@ -1,20 +1,20 @@
-class du implements Comparable <du>
-{
-int k;int v;
-du(int kk,int vv)
-{
-k=kk;
-v=vv;
+// class du implements Comparable <du>
+// {
+// int k;int v;
+// du(int kk,int vv)
+// {
+// k=kk;
+// v=vv;
 
-}
-public int compareTo(du t)
-{
-if(v<=t.v)
-return -1;
-else
-return 1;
-}
-}
+// }
+// public int compareTo(du t)
+// {
+// if(v<=t.v)
+// return -1;
+// else
+// return 1;
+// }
+// }
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap <Integer,Integer> h=new HashMap<Integer,Integer>();
@@ -25,17 +25,19 @@ class Solution {
             else
                h.put(nums[i],h.get(nums[i])+1); 
         }
-        PriorityQueue <du> p=new PriorityQueue<du>();
-        for(Map.Entry<Integer,Integer> e: h.entrySet())
+        // PriorityQueue <du> p=new PriorityQueue<du>();
+        PriorityQueue <Integer> p=new PriorityQueue<>((n1,n2) -> h.get(n1)-h.get(n2));
+        for(int e: h.keySet())
         {
-        p.add(new du(e.getKey(),e.getValue()));
+        p.add(e);
+            if (p.size() > k) p.poll();
         }
         int res[]=new int[k];
-        for(int i=0;i<k;i++)
+        for(int i=k-1;i>=0;i--)
         {
-        res[i]=p.poll().v;
+        res[i]=p.poll();
         }
-        
+        //System.out.println(h.get(2));
        return res;
     }
 }
