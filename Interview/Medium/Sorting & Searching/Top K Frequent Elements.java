39b4e30ffbaa9059aad5fb46ebe391d4182f39f7
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-18 15:26:13 +0530

    Update Top K Frequent Elements.java

--- Interview/Medium/Sorting & Searching/Top K Frequent Elements.java
+++ Interview/Medium/Sorting & Searching/Top K Frequent Elements.java
@@ -1,1 +1,43 @@
+// class du implements Comparable <du>
+// {
+// int k;int v;
+// du(int kk,int vv)
+// {
+// k=kk;
+// v=vv;
 
+// }
+// public int compareTo(du t)
+// {
+// if(v<=t.v)
+// return -1;
+// else
+// return 1;
+// }
+// }
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap <Integer,Integer> h=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!h.containsKey(nums[i]))
+            h.put(nums[i],1);
+            else
+               h.put(nums[i],h.get(nums[i])+1); 
+        }
+        // PriorityQueue <du> p=new PriorityQueue<du>();
+        PriorityQueue <Integer> p=new PriorityQueue<>((n1,n2) -> h.get(n1)-h.get(n2));
+        for(int e: h.keySet())
+        {
+        p.add(e);
+            if (p.size() > k) p.poll();
+        }
+        int res[]=new int[k];
+        for(int i=k-1;i>=0;i--)
+        {
+        res[i]=p.poll();
+        }
+        //System.out.println(h.get(2));
+       return res;
+    }
+}
