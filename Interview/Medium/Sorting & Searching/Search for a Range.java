63e3a3118c5404be8e41a94ee6450ecec454d9d8
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-22 00:49:14 +0530

    Update Search for a Range.java

--- Interview/Medium/Sorting & Searching/Search for a Range.java
+++ Interview/Medium/Sorting & Searching/Search for a Range.java
@@ -1,34 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n=nums.length;
         int res[]=new int[2];
-        res[0]=-1;
-        res[1]=-1;
-        if(nums.length<=0) return res;
-     int s=nums[0];
-        int l=nums[1];
-        if(target<s||target>l) return res;
-        int mid=n/2;int a=0;int b=0;
+        res[0]=find_s(nums,target);
+        res[1]=find_l(nums,target);
+        return res;
+    }
+    int find_s(int[] nums, int target)
+    {
+        int index=-1;
+        int s=0;int l=nums.length-1;
         while(s<=l)
         {
-            if(nums[mid]>target)
+            int mid=s+(l-s)/2;
+            if(nums[mid]>=target)
                 l=mid-1;
-            else if(nums[mid]<target)
+            else if(nums[mid]<=target)
+                s=mid+1;
+            if(nums[mid]==target) index=mid;
+        }   
+        return index;
+    }
+    int find_l(int[] nums, int target)
+    {
+        int index=-1;
+        int s=0;int l=nums.length-1;
+        while(s<=l)
+        {
+            int mid=s+(l-s)/2;
+            
+            if(nums[mid]<=target)
                 s=mid+1;
             else
-            {
-                if(nums[s]<target)
-                    s++;
-                if(nums[l]>target)
-                    l--;
-                 else if(nums[s]==target&&nums[l]==target)
-                 {
-                     res[0]=s;
-                     res[1]=l;
-                    return res;
-                 }
-             }
-        }
-        return res;
+                l=mid-1;
+            if(nums[mid]==target) index=mid;
+        }   
+        return index;
     }
 }
