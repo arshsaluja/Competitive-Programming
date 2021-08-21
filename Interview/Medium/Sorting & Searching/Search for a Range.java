34dcb81748843a679567a6da422c9265c0bfdcef
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-08-21 23:55:10 +0530

    Update Search for a Range.java

--- Interview/Medium/Sorting & Searching/Search for a Range.java
+++ Interview/Medium/Sorting & Searching/Search for a Range.java
@@ -1,5 +1,34 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
+        int n=nums.length;
+        int res[]=new int[2];
+        res[0]=-1;
+        res[1]=-1;
+        if(nums.length<=0) return res;
+     int s=nums[0];
+        int l=nums[1];
+        if(target<s||target>l) return res;
+        int mid=n/2;int a=0;int b=0;
+        while(s<=l)
+        {
+            if(nums[mid]>target)
+                l=mid-1;
+            else if(nums[mid]<target)
+                s=mid+1;
+            else
+            {
+                if(nums[s]<target)
+                    s++;
+                if(nums[l]>target)
+                    l--;
+                 else if(nums[s]==target&&nums[l]==target)
+                 {
+                     res[0]=s;
+                     res[1]=l;
+                    return res;
+                 }
+             }
+        }
+        return res;
     }
 }
