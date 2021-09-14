e782e4d926e83eacab182a4f84d7a82c6ca2be40
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-09-14 23:49:24 +0530

    Update Jump Game.java

--- Interview/Medium/Dynamic Programming/Jump Game.java
+++ Interview/Medium/Dynamic Programming/Jump Game.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
+        int n=nums.length;
+        if(nums[0]==0&&n>=2)
+            return false;
+        int temp=0;
+        for(int i=0;i<n;i=i+temp)
+        {
+            if(i==n-1)
+                return true;
+            if(nums[i]==0)
+                return false;
+            temp=nums[i];
+        }
+       return true; 
     }
 }
-
