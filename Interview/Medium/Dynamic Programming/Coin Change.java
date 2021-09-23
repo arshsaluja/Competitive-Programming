305dcd5eaf4a9be6641427588ce0aee19413ee7e
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-09-23 14:44:23 +0530

    Update Coin Change.java

--- Interview/Medium/Dynamic Programming/Coin Change.java
+++ Interview/Medium/Dynamic Programming/Coin Change.java
@@ -1,5 +1,20 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        
+        Arrays.sort(coins);
+        int c=0;
+        int n=coins.length;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(amount==0)
+                return c;
+            while(coins[i]<=amount)
+            {
+                amount=amount-coins[i];
+                c++;
+            }
+        }
+        if(amount>0)
+            return -1;
+        else return c;
     }
 }
