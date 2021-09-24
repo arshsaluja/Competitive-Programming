cc03b17fb813dee3c52b33f85eeb01570c48798f
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-09-24 19:51:48 +0530

    Update Longest Increasing Subsequence.java

--- Interview/Medium/Dynamic Programming/Longest Increasing Subsequence.java
+++ Interview/Medium/Dynamic Programming/Longest Increasing Subsequence.java
@@ -1,5 +1,24 @@
+// Input: nums = [10,9,2,5,3,7,101,18]
+// Output: 4
+// Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        //Enter your code here..
+        int n=nums.length;int max=0;
+        for(int i=0;i<n;i++)
+        {
+         int c=1;ArrayList<Integer> a=new ArrayList<>();
+         a.add(nums[i]);
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]<nums[j]&&nums[j-1]<nums[j]&&nums[j]<a.get(c))
+                {
+                    a.add(nums[j]);
+                    c++;
+                }
+            }
+         if(c>max)
+             max=c;
+        }
+        return max;
     }
 }
