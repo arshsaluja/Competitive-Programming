d74401c4f29ab37c189b657dad73fd2610c0274c
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-09-29 21:01:22 +0530

    Update Happy Number.java

--- Interview/Medium/Math/Happy Number.java
+++ Interview/Medium/Math/Happy Number.java
@@ -1,26 +1,29 @@
+// Input: n = 19
+// Output: true
+// Explanation:
+// 12 + 92 = 82
+// 82 + 22 = 68
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1)
-            return true;
-     if(n<10&n!=1)
-         return false;
-        while(n>9)
-        {
-            int sq=0;int sum=0;
-            while(n>0)
-            {
-                int c=n%10;
-                sq=c*c;
-                sum+=sq;
-                n=n/10;
-                System.out.println("sum"+sum);
-                 System.out.println(" n "+n);
-            }
-                n=sum;
-            if(n==1)
-                return true;
-        }
-        return true;
         
+        Set <Integer> s=new HashSet<Integer>();
+        while(s.add(n))
+        {
+        int sum=0;
+        while(n>0)
+        {
+            int c=n%10;
+            int sq=c*c;
+             sum+=sq;
+            n=n/10;
+        }
+            if(sum==1)
+                return true;
+            
+            n=sum;
+        }
+        return false;
     }
 }
