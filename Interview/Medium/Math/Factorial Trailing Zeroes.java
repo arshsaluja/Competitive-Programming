e560d8e880a1c019366c4416b621a311c5693ef8
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-09-30 09:32:17 +0530

    Update Factorial Trailing Zeroes.java

--- Interview/Medium/Math/Factorial Trailing Zeroes.java
+++ Interview/Medium/Math/Factorial Trailing Zeroes.java
@@ -1,6 +1,11 @@
-
 class Solution {
-    public boolean isHappy(int n) {
-        
+    public int trailingZeroes(int n) {
+        int count=0;
+        while(n>0)
+        {
+            n=n/5;
+            count+=n;
+        }
+        return count;
     }
 }
