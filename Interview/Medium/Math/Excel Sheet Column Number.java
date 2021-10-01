af819159ed5763ec77a03c35404a298f6822fa2d
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-10-01 21:44:25 +0530

    Update Excel Sheet Column Number.java

--- Interview/Medium/Math/Excel Sheet Column Number.java
+++ Interview/Medium/Math/Excel Sheet Column Number.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int titleToNumber(String co) {
+        int res=0;
+       for(int i=0;i<co.length();i++)
+       {
+           char v=co.charAt(i);
+           int d=v-'A'+1;
+           res=res*26+d;
+       }
+        return res;
+    }
+}
