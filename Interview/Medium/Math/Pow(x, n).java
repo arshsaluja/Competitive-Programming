18538c216b47fdeda834a820631189e883f3d91e
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-10-08 23:37:06 +0530

    Update Pow(x, n).java

--- Interview/Medium/Math/Pow(x, n).java
+++ Interview/Medium/Math/Pow(x, n).java
@@ -1,6 +1,12 @@
 class Solution {
+    public double rec(double x,int n)
+    {
+        if(n==0)
+            return 1;
+        return x*rec(x,n-1);
+        
+    }
     public double myPow(double x, int n) {
-        //write your code here
-        return Math.pow(x,n);
+     return rec( x, n);   
     }
 }
