bbc5db9125b1210540c254339dbf6688abe83469
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-10-13 13:59:15 +0530

    Update Evaluate Reverse Polish Notation.java

--- Interview/Medium/Others/Evaluate Reverse Polish Notation.java
+++ Interview/Medium/Others/Evaluate Reverse Polish Notation.java
@@ -1,5 +1,38 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        //Enter your code
+        Stack <Integer> s=new Stack<>();
+     for(String str:tokens)
+     {
+         if(str.equals("+"))
+         {
+             int a=s.pop();
+             int b=s.pop();
+             s.add(a+b);
+         }
+         else if(str.equals("-"))
+         {
+              int a=s.pop();
+             int b=s.pop();
+             s.add(b-a);
+         }
+         else if(str.equals("*"))
+         {
+              int a=s.pop();
+             int b=s.pop();
+             s.add(a*b);
+         }
+         else if(str.equals("/"))
+         {
+              int a=s.pop();
+             int b=s.pop();
+             s.add(b/a);
+         }
+         else
+         {
+             s.add(Integer.parseInt (str));
+         }
+         
+     }
+        return s.pop();
     }
 }
