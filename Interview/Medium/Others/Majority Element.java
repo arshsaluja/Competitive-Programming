bc7c3680cd87721c78370e327f5d2c4406bef8c9
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2021-10-14 17:57:24 +0530

    Update Majority Element.java

--- Interview/Medium/Others/Majority Element.java
+++ Interview/Medium/Others/Majority Element.java
@@ -1,5 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
+        HashMap <Integer,Integer> h=new HashMap<>();
+        for(int i:nums)
+        {
+            if(h.containsKey(i))
+            {
+                h.put(i,h.get(i)+1);
+            }
+            else
+                h.put(i,1);
+        }
+         Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : h.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
     }
 }
