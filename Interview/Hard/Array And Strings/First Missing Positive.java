4aba0194b8e458b23e371408d2a6634e00c3f4d0
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2022-02-26 20:03:01 +0530

    Update First Missing Positive.java

--- Interview/Hard/Array And Strings/First Missing Positive.java
+++ Interview/Hard/Array And Strings/First Missing Positive.java
@@ -1,1 +1,38 @@
-
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        int l=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>l)
+                l=nums[i];
+                
+        }
+        System.out.println("l: "+l);
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+           if(nums[i]==1)
+           {
+               c=i;
+               System.out.println("c: "+c);
+               break;
+           }
+        }
+        int o=0;
+        for(int i=1;i<=l;i++)
+        {
+            if(nums[c++]!=i)
+            {
+                System.out.println("o: "+o);
+                o=i;
+                break;
+            }
+                
+        }
+        if(o==0)
+            return l+1;
+     return o;   
+    }
+}
