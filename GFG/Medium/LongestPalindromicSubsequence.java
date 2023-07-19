8b673f9222e0d1c108063e9efd5bba10d56be18a
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-07-19 22:10:10 +0530

    Added the code for Longest Palindromic Subsequence

---
+++ GFG/Medium/LongestPalindromicSubsequence.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.longestPalinSubseq(s));
+                }
+                
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int longestPalinSubseq(String S)
+    {
+        //Apply dp -> max of  dp[i-1][j]+dp[i][j-1]+dp[i-1][j-1] if(palindrom of both characters is not there if there the +1  into this )
+        
+    }
+}
