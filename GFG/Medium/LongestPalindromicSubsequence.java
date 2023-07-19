0760249d407670f2586912faa92a3192cc44cfb0
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-07-19 23:18:58 +0530

    Update LongestPalindromicSubsequence.java

--- GFG/Medium/LongestPalindromicSubsequence.java
+++ GFG/Medium/LongestPalindromicSubsequence.java
@@ -26,9 +26,29 @@ class GfG
 
 class Solution
 {
+    
     public int longestPalinSubseq(String S)
     {
-        //Apply dp -> max of  dp[i-1][j]+dp[i][j-1]+dp[i-1][j-1] if(palindrom of both characters is not there if there the +1  into this )
+        int n=S.length();
+        StringBuilder sb=new StringBuilder(S);
+        String rev=sb.reverse().toString();
+        int dp[][]=new int [n+1][n+1];
+        
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i>0 && j>0)
+                {
+                if(S.charAt(i-1)==rev.charAt(j-1))
+                {
+                   dp[i][j]=dp[i-1][j-1]+1;}
+                else
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+            }
+        }
+        return dp[n][n];
         
     }
 }
