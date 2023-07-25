089aab231853008f10a1197f39011b6fd8b5cc05
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-07-25 23:23:51 +0530

    Create LevelOrderTraversalInSpiralForm.java

---
+++ GFG/Easy/LevelOrderTraversalInSpiralForm.java
@@ -0,0 +1,170 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+//Initial Template for Java
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+ 
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+ 
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                Spiral g = new Spiral();
+                ArrayList<Integer> result = g.findSpiral(root);
+                for(int value : result)
+                System.out.print(value + " ");
+			    System.out.println();
+    	        
+	        }
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/*
+// A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Spiral
+{
+    //Function to return a list containing the level order 
+    //traversal in spiral form.	
+    ArrayList<Integer> findSpiral(Node root) 
+    {
+        // Your code here
+         Stack<Node> stack1=new Stack<>();
+         Stack<Node> stack2=new Stack<>();
+        ArrayList<Integer> result =new ArrayList<>();
+        if(root==null) return result;
+        stack1.push(root);
+        
+        while(!stack1.isEmpty()||!stack2.isEmpty())
+        {
+           while (!stack1.isEmpty()) 
+           {
+               Node t=stack1.pop();
+               result.add(t.data);
+               if(t.right !=null)stack2.push(t.right);
+               if(t.left !=null)stack2.push(t.left);
+               
+           }
+          while (!stack2.isEmpty())
+           {
+               Node st=stack2.pop();
+               result.add(st.data);
+               if(st.left !=null)stack1.push(st.left);
+               if(st.right !=null)stack1.push(st.right);
+           }
+        }
+        return result;
+    }
+}
