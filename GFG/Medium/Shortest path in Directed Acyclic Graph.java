cb2abccdc1c36e673e9557417de7a251901d6fee
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-08-03 23:27:45 +0530

    Update Shortest path in Directed Acyclic Graph.java

--- GFG/Medium/Shortest path in Directed Acyclic Graph.java
+++ GFG/Medium/Shortest path in Directed Acyclic Graph.java
@@ -1,4 +1,39 @@
-int dist[] = new int[N];
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int[][] edge = new int[m][3];
+			for (int i = 0; i < m; i++) {
+				edge[i][0] = sc.nextInt();
+				edge[i][1] = sc.nextInt();
+				edge[i][2] = sc.nextInt();
+			}
+			Solution obj = new Solution();
+			int res[] = obj.shortestPath(n, m,edge);
+			for (int i = 0; i < n; i++) {
+				System.out.print(res[i] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+
+	public int[] shortestPath(int N,int M, int[][] edges) {
+		//Code here
+		int dist[] = new int[N];
 Arrays.fill(dist,Integer.MAX_VALUE);
 dist[0]=0;
  
@@ -23,3 +58,5 @@ for(int i=0;i<N;i++)
 }
  
 return dist;
+	}
+}
