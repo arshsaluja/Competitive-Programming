32f637259002bffa89cde9b41e65aba71e0bcb98
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-08-05 19:45:42 +0530

    Create ChocolateDistributionProblem.java

---
+++ GFG/Easy/ChocolateDistributionProblem.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = 0;i<n;i++)
+                {
+                    int x = sc.nextInt();
+                    arr.add(x);
+                }
+            int m = sc.nextInt();
+            
+            Solution ob = new Solution();
+            
+    		System.out.println(ob.findMinDiff(arr,n,m));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        long min=Integer.MAX_VALUE;int j=0;
+        for(int i=m-1;i<n;i++)
+        {
+            min=Math.min(a.get(i)-a.get(j++),min);
+
+        }
+        return min;
+    }
+}
