4e37ce4df07c8f5e6e1c8a6dfe0014b6ecaa1b51
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-08-13 23:07:53 +0530

    Update NthFibonacciNumber.java

--- GFG/Easy/NthFibonacciNumber.java
+++ GFG/Easy/NthFibonacciNumber.java
@@ -1,1 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
 
+            System.out.println(ob.nthFibonacci(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+class Solution {
+   static long nthFibonacci(long n){    
+      int N=(int)n;
+      long dp[]=new long[N+1];
+      dp[0]=0;
+      dp[1]=1;
+      for(int i=2;i<=N;i++)
+      {
+          dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+      }
+      return dp[N];
+   }
+}
