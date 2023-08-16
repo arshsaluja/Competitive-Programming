d6b1293661f99cc47179b9aa7e9e5f7a6ee4e2cf
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-08-16 23:33:57 +0530

    Update NthCatalanNumber.java

--- GFG/Medium/NthCatalanNumber.java
+++ GFG/Medium/NthCatalanNumber.java
@@ -1,1 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
 
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.findCatalan(n);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int findCatalan(int n) {
+        // code here
+        if (n == 0) {
+            return 1;
+        }      
+        final int MOD = (int) (1e9 + 7);
+        long[] v = new long[n + 1];
+        v[0] = v[1] = 1;
+        
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                v[i] = (v[i] + (v[j] * v[i - j - 1]) % MOD) % MOD;
+            }
+        }  
+        return (int) v[n];
+    }
+}
+        
