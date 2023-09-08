9ca5a5132d9b41a7c84e445aa42de3bf438d128b
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-08 23:51:42 +0530

    Update MergeStringsAlternately.java

--- Interview/Revision2023/Leetcode75/MergeStringsAlternately.java
+++ Interview/Revision2023/Leetcode75/MergeStringsAlternately.java
@@ -1,5 +1,14 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-
+        int i=0;StringBuilder str = new StringBuilder();
+        while(i<word1.length()||i<word2.length())
+        {
+            if(i<word1.length())
+            str.append(word1.charAt(i));
+            if(i<word2.length())
+            str.append(word2.charAt(i));
+            i++;
+        }
+        return str.toString();
     }
 }
