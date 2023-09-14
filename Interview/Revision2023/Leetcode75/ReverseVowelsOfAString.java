869b84f2afbdf4a860ccab9e8848a6048766ca68
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-14 18:46:03 +0530

    Update ReverseVowelsOfAString.java

--- Interview/Revision2023/Leetcode75/ReverseVowelsOfAString.java
+++ Interview/Revision2023/Leetcode75/ReverseVowelsOfAString.java
@@ -1,5 +1,23 @@
 class Solution {
     public String reverseVowels(String s) {
-        
+        String str="";int c=0;String y="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+                str=ch+str;
+            }
+        int j=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+                y+=str.charAt(j);
+                j++;
+            }
+            else
+            y+=ch;
+        }
+        return y;
     }
 }
