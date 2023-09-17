f9bf47c5bce84042bde032b2085150343b21adf2
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-17 19:40:57 +0530

    Update IncreasingTripletSubsequence.java

--- Interview/Revision2023/Leetcode75/Medium/IncreasingTripletSubsequence.java
+++ Interview/Revision2023/Leetcode75/Medium/IncreasingTripletSubsequence.java
@@ -1,5 +1,16 @@
 class Solution {
+    //[1,2,3,4,5]
     public boolean increasingTriplet(int[] nums) {
-        
+        int n=nums.length;
+        int i=Integer.MAX_VALUE,k=Integer.MAX_VALUE;
+        for(int j=0;j<n;j++)
+        {
+           if(i>=nums[j])
+           i=nums[j];
+           else if(k>=nums[j])
+           k=nums[j];
+           else return true;
+        }
+        return false;
     }
 }
