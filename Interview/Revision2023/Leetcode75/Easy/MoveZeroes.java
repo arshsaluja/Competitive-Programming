6ec9a733bd4506a8c5a9f7b5e7785f8bbd91c266
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-20 02:49:17 +0530

    Update MoveZeroes.java

--- Interview/Revision2023/Leetcode75/Easy/MoveZeroes.java
+++ Interview/Revision2023/Leetcode75/Easy/MoveZeroes.java
@@ -1,1 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int c=0,k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            c++;
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0)
+            nums[k++]=nums[i];
+        }
+        for(int i=nums.length-c;i<nums.length;i++)
+        {
+            nums[i]=0;
+        }
+        
+        
 
+    }
+}
