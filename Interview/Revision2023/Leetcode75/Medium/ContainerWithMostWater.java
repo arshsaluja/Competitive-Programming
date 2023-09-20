156cb34c0f1dda44d61bc0ea058a559a3d683575
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-20 18:36:52 +0530

    Update ContainerWithMostWater.java

--- Interview/Revision2023/Leetcode75/Medium/ContainerWithMostWater.java
+++ Interview/Revision2023/Leetcode75/Medium/ContainerWithMostWater.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max=Integer.MIN_VALUE;
+        int l=0;
+        int r=height.length-1;
+        while(l<r)
+        {
+            max=Math.max(max,(r-l)*Math.min(height[r],height[l]));
+            if(height[l]<height[r])
+            l++;
+            else
+            r--;
 
+        }
+        return max;
+    }
+}
