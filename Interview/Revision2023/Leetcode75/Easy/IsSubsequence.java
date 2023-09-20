966bbbe324db4629e5b73b347d32ef190562b1c8
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-20 16:58:13 +0530

    Update IsSubsequence.java

--- Interview/Revision2023/Leetcode75/Easy/IsSubsequence.java
+++ Interview/Revision2023/Leetcode75/Easy/IsSubsequence.java
@@ -1,1 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()==0) return true;
 
+        int j=0;
+        for(int i=0;i<t.length();i++)
+        {
+            char ch=t.charAt(i);
+            char c2=s.charAt(j);
+            if(ch==c2)
+            {
+                j++;
+            }
+            if(j>=s.length()) return true;
+        }
+        return false;
+    }
+}
