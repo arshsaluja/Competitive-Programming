2b46f0a6ec13ab96d644292405e8512a8ce19867
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-20 20:32:27 +0530

    Update MaxNumberOfK-SumPairs.java

--- Interview/Revision2023/Leetcode75/Medium/MaxNumberOfK-SumPairs.java
+++ Interview/Revision2023/Leetcode75/Medium/MaxNumberOfK-SumPairs.java
@@ -1,5 +1,17 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        
+        int operation=0;
+        for(int i=0;i<nums.length;i++)
+        {
+        for(int j=i+1;j<nums.length;j++)
+        {
+            if(nums[i]!=0&&nums[j]!=0&&nums[i]+nums[j]==k){
+                nums[i]=0;
+                nums[j]=0;
+            operation++;
+            }
+        }
+        }
+        return operation;
     }
 }
