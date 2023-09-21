5b5ab62625b2727c51d339567455facacdb309a5
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-21 20:53:50 +0530

    Update MaximumAverageSubarrayI.java

--- Interview/Revision2023/Leetcode75/Sliding Window/Easy/MaximumAverageSubarrayI.java
+++ Interview/Revision2023/Leetcode75/Sliding Window/Easy/MaximumAverageSubarrayI.java
@@ -1,5 +1,19 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        
+        int n=nums.length;
+        double avg=Integer.MIN_VALUE;
+        if(k==1 && n==1) avg=nums[0];
+     for(int i=0;i<n-k+1;i++)
+     {
+         int t=0,sum=0;
+         while(t<k)
+         {
+             sum+=nums[i+t];
+             t++;
+         }
+         double average=(double)sum/k;
+         avg=Math.max(avg,average);
+     } 
+       return avg;
     }
 }
