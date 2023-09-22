f1e0642bdfc1846c84e60c58d15897be7e604d98
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-09-22 23:42:31 +0530

    Update MaximumNumberOfVowelsInASubstringOfGivenLength.java

--- Interview/Revision2023/Leetcode75/Sliding Window/Medium/MaximumNumberOfVowelsInASubstringOfGivenLength.java
+++ Interview/Revision2023/Leetcode75/Sliding Window/Medium/MaximumNumberOfVowelsInASubstringOfGivenLength.java
@@ -1,6 +1,23 @@
 class Solution {
+    public int subs(String str)
+    {
+        int c=0;
+        for(int i=0;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+            c++;
+        }
+        return c;
+    }
     public int maxVowels(String s, int k) {
-
-        
+        int r=k-1;int max=0;
+        for(int i=0;i<s.length()-k+1;i++)
+        {
+                String str=s.substring(i,i+k);
+                int c=subs(str);
+                max=Math.max(max,c);
+        }
+        return max;
     }
 }
