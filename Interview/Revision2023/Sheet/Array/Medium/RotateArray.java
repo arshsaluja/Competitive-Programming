08c2bf35c52f889bceb5c7b4ff94d57fae522e13
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-03 19:55:27 +0530

    Update RotateArray.java

--- Interview/Revision2023/Sheet/Array/Medium/RotateArray.java
+++ Interview/Revision2023/Sheet/Array/Medium/RotateArray.java
@@ -1,5 +1,20 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        int n=nums.length;
+        if(k > n)
+        k=k%n;
+        int [] ar=new int[n];
+        int j=0;
+
+        for(int i=0;i<k;i++)
+        {
+            ar[i]=nums[n-k+i];
+        }
+        for(int i=k;i<n;i++)
+        {
+            ar[i]=nums[j];
+            j++;
+        }
+        System.arraycopy( ar, 0, nums, 0, nums.length );
     }
 }
