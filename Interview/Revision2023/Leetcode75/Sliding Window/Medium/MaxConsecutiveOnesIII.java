4aa5eaecb3d5c0e53d435a6894bd18774011e188
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-12 23:49:05 +0530

    Update MaxConsecutiveOnesIII.java

--- Interview/Revision2023/Leetcode75/Sliding Window/Medium/MaxConsecutiveOnesIII.java
+++ Interview/Revision2023/Leetcode75/Sliding Window/Medium/MaxConsecutiveOnesIII.java
@@ -1,7 +1,12 @@
-
-class Sol {
+class Solution {
     public int longestOnes(int[] nums, int k) {
-        
+        int conc=0;
+        int max=0;int l=0;int r=0;
+        for(int i=0;i<nums.length;i++)
+        {
+         if(nums[i]==   1)   {
+             
+         }
+        }
     }
-    
 }
