c78c6a4ae5da34b3c6a4659df5b7ffd85318b187
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-13 19:55:34 +0530

    Update FindTheHighestAltitude.java

--- Interview/Revision2023/Leetcode75/PrefixSum/Easy/FindTheHighestAltitude.java
+++ Interview/Revision2023/Leetcode75/PrefixSum/Easy/FindTheHighestAltitude.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=Math.max(0,gain[0]);int j=0;
+        
+        for(int i=1;i<gain.length;i++)
+        {
+            gain[i]=gain[i]+gain[i-1];
+            max=Math.max(max,gain[i]);
+        }
+        
+        return max;
+    }
+}
