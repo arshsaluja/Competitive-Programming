47f5db908d04e41e9546864e977b7bafb5b38035
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-13 18:33:15 +0530

    Update LongestSubarrayOf1'sAfterDeletingOneElement.java

--- Interview/Revision2023/Leetcode75/Sliding Window/Medium/LongestSubarrayOf1'sAfterDeletingOneElement.java
+++ Interview/Revision2023/Leetcode75/Sliding Window/Medium/LongestSubarrayOf1'sAfterDeletingOneElement.java
@@ -1,5 +1,19 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        
+        int i=0;int j=0;int k=1;
+        while(j<nums.length)
+        {
+            if(nums[j]==0)
+            k--;
+            if(k<0)
+            {
+                if(nums[i]==0)
+                k++;
+            i++;
+
+            }
+            j++;
+        }
+        return j-i-1;
     }
 }
