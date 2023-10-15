392e02ebd73477e4504c7d3c15445970a6850ed0
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-15 09:13:44 +0530

    Update FindPivotIndex.java

--- Interview/Revision2023/Leetcode75/PrefixSum/Easy/FindPivotIndex.java
+++ Interview/Revision2023/Leetcode75/PrefixSum/Easy/FindPivotIndex.java
@@ -1,22 +1,15 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        if(nums.length==1) return 0;
+        int total=0;
+        for(int i:nums)
+        total+=i;
 
-        int k=0;
+        int lsum=0;int rsum=0;
         for(int i=0;i<nums.length;i++)
         {
-            int l=i;int r=i;int lsum=0;int rsum=0;
-            for(int j=0;j<i;j++)
-            {
-                lsum+=nums[j];
-                k++;
-            }
-            for(int j=i+1;j<nums.length;j++)
-            {
-                rsum+=nums[j];
-                k++;
-            }
-            if(lsum==rsum&&k>0) return i;
+            rsum= total-lsum-nums[i];
+            if(lsum==rsum) return i;
+            lsum+=nums[i];
         }
         return -1;
     }
