e3d296490e29cbf17508e6a272729d5eb8d448b5
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-15 16:12:55 +0530

    Update UniqueNumberOfOccurrences.java

--- Interview/Revision2023/Leetcode75/HashMapSet/Easy/UniqueNumberOfOccurrences.java
+++ Interview/Revision2023/Leetcode75/HashMapSet/Easy/UniqueNumberOfOccurrences.java
@@ -1,20 +1,9 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Arrays.sort(arr);
-        Set<Integer> key=new HashSet<>();
-        Set<Integer> value=new HashSet<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
 
-        for(int i:arr) key.add(i);
-        for(int s:key){
-            int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(s==arr[i]) count++;
-            else if(count>0) break;
-        }
-        value.add(count);
-        }
-        if(key.size()==value.size()) return true;
-        return false;
-        
+        for(int i:arr) map.put(i,map.getOrDefault(i,0)+1);
+        HashSet<Integer> set=new HashSet<>(map.values());
+        return set.size()==map.size();
     }
 }
