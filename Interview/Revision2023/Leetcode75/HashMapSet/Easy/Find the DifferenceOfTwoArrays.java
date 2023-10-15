278d0bcb9f1dcb6644bdc4b02f8d8e635371a081
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-15 09:39:15 +0530

    Update Find the DifferenceOfTwoArrays.java

--- Interview/Revision2023/Leetcode75/HashMapSet/Easy/Find the DifferenceOfTwoArrays.java
+++ Interview/Revision2023/Leetcode75/HashMapSet/Easy/Find the DifferenceOfTwoArrays.java
@@ -1,5 +1,24 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        
+     Set<Integer> s1=new HashSet<>();
+     Set<Integer> s2=new HashSet<>();
+     for(int i:nums1)
+     s1.add(i);   
+     for(int i:nums2)
+     s2.add(i);
+
+List<Integer> a1=new ArrayList<>();
+     for(int i:s1){
+         if(!s2.contains(i)) a1.add(i);
+     }
+List<Integer> a2=new ArrayList<>();
+     for(int i:s2){
+         if(!s1.contains(i)) a2.add(i);
+     }
+ List<List<Integer>> res=new ArrayList<>();   
+ res.add(a1);
+ res.add(a2);
+  
+  return res;
     }
 }
