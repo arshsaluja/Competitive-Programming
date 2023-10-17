ff2033d076a647a717d730fba35eaabdb89f5f43
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-17 23:37:54 +0530

    Update DetermineIfTwoStringsAreClose.java

--- Interview/Revision2023/Leetcode75/HashMapSet/Medium/DetermineIfTwoStringsAreClose.java
+++ Interview/Revision2023/Leetcode75/HashMapSet/Medium/DetermineIfTwoStringsAreClose.java
@@ -1,5 +1,16 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
+        int s1=word1.length();
+        int s2=word2.length();
+        if(s1!=s2) return false;
+        Set <Character> s1=new HashSet<>();
+        Set <Character> s2=new HashSet<>();
         
+
+        for(int i=0;i<s1;i++){
+                if(ar1[i]!=ar2[i]) return false;
+        }
+
+        return true;
     }
 }
