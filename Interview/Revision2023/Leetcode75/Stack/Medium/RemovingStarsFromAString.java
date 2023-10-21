ed1b7e92ddc50b9b8e0384604f115d1e67e25538
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-21 23:02:29 +0530

    Update RemovingStarsFromAString.java

--- Interview/Revision2023/Leetcode75/Stack/Medium/RemovingStarsFromAString.java
+++ Interview/Revision2023/Leetcode75/Stack/Medium/RemovingStarsFromAString.java
@@ -1,8 +1,6 @@
 class Solution {
     public String removeStars(String s) {
-        String str="";
        Stack<Character> st=new Stack<>();
-       Stack<Character> st2=new Stack<>();
            for(int i=0;i<s.length();i++){
                char ch=s.charAt(i);
                if(ch=='*'){
@@ -10,15 +8,11 @@ class Solution {
                }
                else st.push(ch);
            }
-           System.out.println(st);
-           while(!st.isEmpty()){
-               st2.push(st.peek());
-               st.pop();
-           }
-           while(!st2.isEmpty()){
-               str+=st2.peek();
-               st2.pop();
-           }
-           return str;
+           StringBuilder sb=new StringBuilder();
+
+           for(char c:st)
+           sb.append(c);
+
+           return sb.toString();
     }
 }
