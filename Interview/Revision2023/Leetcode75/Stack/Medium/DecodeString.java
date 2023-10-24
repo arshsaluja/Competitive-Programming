7d0f2e227b3a6835c3a70ac9906b6ecaa0b24235
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-10-24 23:50:15 +0530

    Update DecodeString.java

--- Interview/Revision2023/Leetcode75/Stack/Medium/DecodeString.java
+++ Interview/Revision2023/Leetcode75/Stack/Medium/DecodeString.java
@@ -1,5 +1,14 @@
 class Solution {
     public String decodeString(String s) {
-        
+        Stack<Character> st=new Stack<>();
+        for(char c:s){
+            if(c=='[') 
+            else if(c==']')
+            else 
+            {
+                int no=c;
+
+            }
+        }
     }
 }
