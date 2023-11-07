ddf478e67cd1607d8920676237b3713eae1f77e2
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-11-08 01:21:41 +0530

    Update ProductOfArrayExceptSelf.java

--- Interview/Revision2023/Interview/ProductOfArrayExceptSelf.java
+++ Interview/Revision2023/Interview/ProductOfArrayExceptSelf.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int ans[]=new int[n];
+        ans[0]=1;
+        for (int i=1;i<n;i++){
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int product=1;
+        
+        for (int i=n-1;i>=0;i--){
+            ans[i]=ans[i]*product;
+            product*=nums[i];
+        }
+        return ans;
+        
+    }
+}
