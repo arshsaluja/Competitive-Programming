e7ea1995ab67d83e30e16860a3714ac96085260d
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-11-12 05:04:44 +0530

    Update 4SumII.java

--- Interview/Revision2023/Interview/4SumII.java
+++ Interview/Revision2023/Interview/4SumII.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer,Integer> map=new HashMap<>();
+        
+        for(int a:nums1){
+            for(int b:nums2){
+                map.put(a+b,map.getOrDefault(a+b,0)+1);
+            }
+        }
+        int co=0;
+        for(int c:nums3){
+            for(int d:nums4){
+                co+=map.getOrDefault(-(c+d),0);
+            }
+        }
+        return co;
+    }
+}
