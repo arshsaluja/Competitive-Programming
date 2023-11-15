7021144a959bbfdb1532d2eaa8dd05bb694aa890
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-11-15 21:39:53 +0530

    Update FirstMissingPositive.java

--- Interview/Revision2023/Interview/FirstMissingPositive.java
+++ Interview/Revision2023/Interview/FirstMissingPositive.java
@@ -1,5 +1,20 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        int next=n+1;
+        for(int i=0;i<n;i++){if(nums[i]<=0||nums[i]>n) nums[i]=next;}
         
+        for(int i=0;i<n;i++){
+            int num=Math.abs(nums[i]);
+            if(num== next) continue;
+            num--;
+            if(nums[num]>0){
+                nums[num]*=-1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0) return i+1;
+        }
+        return next;
     }
 }
