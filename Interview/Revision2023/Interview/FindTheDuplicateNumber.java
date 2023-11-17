293bd6a2d6b7d44b65326e21dfebcbfa05f150db
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-11-17 16:02:03 +0530

    Update FindTheDuplicateNumber.java

--- Interview/Revision2023/Interview/FindTheDuplicateNumber.java
+++ Interview/Revision2023/Interview/FindTheDuplicateNumber.java
@@ -1,5 +1,16 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        int slow=0,fast=0,slow2=0;
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast) break;
+        }
         
+        while(true){
+            slow=nums[slow];
+            slow2=nums[slow2];
+            if(slow==slow2) return slow;
+        }
     }
 }
