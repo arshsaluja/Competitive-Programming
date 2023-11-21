67b93b342b258136873907238101b5f02ccd5e24
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-11-21 23:12:31 +0530

    Update MinimumWindowSubstring.java

--- Interview/Revision2023/Interview/MinimumWindowSubstring.java
+++ Interview/Revision2023/Interview/MinimumWindowSubstring.java
@@ -1,9 +1,19 @@
 class Solution {
     public String minWindow(String s, String t) {
         int l=0;int r=0;
-        int tl=t.length();
-        for(int i=0;i<t.length;i++){
-            
+        int t1=t.length();
+//         Input: s = "ADOBECODEBANC", t = "ABC"
+// Output: "BANC"
+// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+        HashMap<Character,Integer> th=new HashMap<>();
+        HashMap<Character,Integer> sh=new HashMap<>();
+        for(int i=0;i<t1;i++){
+            th.put(t.charAt(i),th.getOrDefault(t.charAt(i),0)+1);
         }
+        System.out.println(th);
+        while(l<=s.length()){
+            sh.put(s.charAt(l),sh.getOrDefault(s.charAt(l),0)+1);
+        }
+        return "";
     }
 }
