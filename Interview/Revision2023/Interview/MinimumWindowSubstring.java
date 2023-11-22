6b8a083088be8be9a1f72bc02d06135d51837d63
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-11-22 12:46:34 +0530

    Update MinimumWindowSubstring.java

--- Interview/Revision2023/Interview/MinimumWindowSubstring.java
+++ Interview/Revision2023/Interview/MinimumWindowSubstring.java
@@ -1,19 +1,38 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l=0;int r=0;
+        int st=0;int end=0;
         int t1=t.length();
-//         Input: s = "ADOBECODEBANC", t = "ABC"
-// Output: "BANC"
-// Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
-        HashMap<Character,Integer> th=new HashMap<>();
-        HashMap<Character,Integer> sh=new HashMap<>();
+        HashMap<Character,Integer> map=new HashMap<>();
+      
         for(int i=0;i<t1;i++){
-            th.put(t.charAt(i),th.getOrDefault(t.charAt(i),0)+1);
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
         }
-        System.out.println(th);
-        while(l<=s.length()){
-            sh.put(s.charAt(l),sh.getOrDefault(s.charAt(l),0)+1);
+        
+        int count=0,min=Integer.MAX_VALUE,head=0;
+        while(end<s.length()){
+            char ch=s.charAt(end);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch)>=0) count++;
+                
+            }
+            end++;
+            
+            while(count==t.length()){
+                if(min>end-st){
+                    min=end-(head=st);
+                }
+                char sch=s.charAt(st);
+                if(map.containsKey(sch)){
+                    map.put(sch,map.get(sch)+1);
+                    if(map.get(sch)>0) count --;
+                }
+                st++;
+                
+            }
         }
+        if(min==Integer.MAX_VALUE)
         return "";
+        return s.substring(head,head+min);
     }
 }
