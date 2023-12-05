d2ffbf54632061531593303600e42d1f79c46fd9
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2023-12-05 23:08:55 +0530

    Update LongestCommonSubsequence.java

--- Interview/Revision2023/Interview/LongestCommonSubsequence.java
+++ Interview/Revision2023/Interview/LongestCommonSubsequence.java
@@ -1,5 +1,15 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        
+        int m=text1.length();
+        int n=text2.length();
+        return rec(text1,text2,m,n);
+    }
+    public int rec(String s, String t,int m,int n) {
+        if(m==0||n==0) return 0;
+        else if(s.charAt(m-1)==t.charAt(n-1))
+        return 1+rec(s,t,m-1,n-1);
+        else
+        return Math.max(rec(s,t,m-1,n),
+        rec(s,t,m,n-1));
     }
 }
